3b001ec8018aa47aaf9575f729062fce3cfe837a
coreyderosa <coreyderosa@gmail.com>
2016-08-11 21:03:17 -0600

    added code that will hide or slide some text behind or below h1 Text

--- JavaScript2.0.js
+++ JavaScript2.0.js
@@ -1,6 +1,10 @@
 // JavaScript template
-$(document).ready(function() {
+$(document).ready(function() {   //This template will help the JavaScript code begin as soon as HTML page is done loading.
 
+    $("p").hide();
 
+    $("h1").click(function () {
+        $(this).next().slideToggle(300);
+    });
 
 });
