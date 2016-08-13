230574461ac1a73c65d504b6aa6a1a5e758ab595
coreyderosa <coreyderosa@gmail.com>
2016-08-13 12:36:35 -0600

    Added .js file
    JavaScript2.1- will change background when testbutton is clicked

---
+++ JavaScript2.1.js
@@ -0,0 +1,12 @@
+// JavaScript template- This template will help the JavaScript code begin as soon as HTML page is done loading.
+
+$(document).ready(function() {   
+
+	$("#testbutton").click(function() {
+		
+		$("div").css("background-color","red");
+		
+	});
+
+
+});
