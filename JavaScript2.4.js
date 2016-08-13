6e8b9b7c7afea1df370438d85a7d01831749a1df
coreyderosa <coreyderosa@gmail.com>
2016-08-13 15:44:09 -0600

    Left animate event increments
    "+=100px" allows element to move left in increments of 100px per click

--- JavaScript2.4.js
+++ JavaScript2.4.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 			
 		"font-size": "3em",
 		"width": "50%",
-		"left": "100px" //element must have position property value of relative, fixed or absolute
+		"left": "+=100px" //element must have position property value of relative, fixed or absolute
 		
 		}, 1000);
 		
