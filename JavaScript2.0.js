df17c3f4917ea746e1518d532abfc462b16f3e18
coreyderosa <coreyderosa@gmail.com>
2016-08-13 12:23:34 -0600

    Updated comment
    moved comments for easy reading

--- JavaScript2.0.js
+++ JavaScript2.0.js
@@ -1,8 +1,10 @@
-// JavaScript template
-$(document).ready(function() {   //This template will help the JavaScript code begin as soon as HTML page is done loading.
-
+// JavaScript template- This template will help the JavaScript code begin as soon as HTML page is done loading.
+$(document).ready(function() {   
+	
+	//hides the p tags
     $("p").hide();
 
+	
     $("h1").click(function () {
         $(this).next().slideToggle(300);
     });
