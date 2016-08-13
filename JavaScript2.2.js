f4574133fb33f762c06a061d2a1127470225674a
coreyderosa <coreyderosa@gmail.com>
2016-08-13 14:18:14 -0600

    Mouseleave/Mouseenter
    -when mouse icon hovers over element background turns red
    -when mouse icon no longer hovers over element background turns yellow

--- JavaScript2.2.js
+++ JavaScript2.2.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 						   
-  $("h1").mousedown(function() {
+  $("h1").mouseenter(function() {
     $(this).css("background-color","red");
   });
   
-    $("h1").mouseup(function() {
+    $("h1").mouseleave(function() {
     $(this).css("background-color","yellow");
   });
   
