53375b7e96788d1aa0ce3d6710a46ba32d75a98c
coreyderosa <coreyderosa@gmail.com>
2016-08-13 15:53:01 -0600

    Adding effect after animation
    h3 will fade out after h2 animation

--- JavaScript2.4.js
+++ JavaScript2.4.js
@@ -8,7 +8,11 @@ $(document).ready(function() {
 		"width": "50%",
 		"left": "+=100px" //element must have position property value of relative, fixed or absolute
 		
-		}, 1000);
+		}, 1000, function() {
+			
+			$("h3").fadeToggle(1000);
+			
+		});
 		
 	});
 
