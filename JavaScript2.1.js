14e55be3260b0153eb5ecec3dcaf1607d9763ed9
coreyderosa <coreyderosa@gmail.com>
2016-08-13 13:16:08 -0600

    added this as selector
    this is a keyword.  Do not incase in quotes.  this will affect whatever
    element is clicked, but it will only affect the selectors located in the
    click.(function()...so in this case the 'Test' button and any element
    that has a <strong> attribute

--- JavaScript2.1.js
+++ JavaScript2.1.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 	$("#testbutton, strong").click(function() {
 		
-		$("strong.multiple").css("background-color","red");
+		$(this).css("background-color","red");
 		
 	});
 
