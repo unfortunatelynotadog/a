ebdbec96a98f9e4a0659d5d3160986a1bb6b8ac0
coreyderosa <coreyderosa@gmail.com>
2016-08-13 14:52:41 -0600

    $("h2").fadeTo(1000, 0.3);
    h2 fades to a 30% opacity within 1 second whenever h1 is clicked

--- JavaScript2.3.js
+++ JavaScript2.3.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
-    $("h2").hide();
+    //$("h2").hide();
 	
   $("h1").click(function() {
-     $("h2").fadeToggle(1000);
+     $("h2").fadeTo(1000, 0.3);
 	
   });
   
