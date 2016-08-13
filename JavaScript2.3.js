bc236575e574cc4b0d585247660305f79867d795
coreyderosa <coreyderosa@gmail.com>
2016-08-13 14:59:17 -0600

    $("h2").hide(1000, function() {$("h3").fadeOut(1000)});
    whenever h1 is clicked h2 is hidden then h3 will fade out after h2 is
    hidden

--- JavaScript2.3.js
+++ JavaScript2.3.js
@@ -3,7 +3,10 @@ $(document).ready(function() {
     //$("h2").hide();
 	
   $("h1").click(function() {
-     $("h2").delay(2000).fadeToggle(1000);
+     $("h2").hide(1000, function() {
+		 $("h3").fadeOut(1000)
+		 
+	 });
 	
   });
   
