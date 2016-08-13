fe68c1ff174e997e5b2f2663b7bd8c3f1bc0abf3
coreyderosa <coreyderosa@gmail.com>
2016-08-13 13:09:20 -0600

    added strong to multiple selector
    affects only strong attributes where class="multiple" in HTML

--- JavaScript2.1.js
+++ JavaScript2.1.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 	$("#testbutton").click(function() {
 		
-		$(".multiple").css("background-color","red");
+		$("strong.multiple").css("background-color","red");
 		
 	});
 
