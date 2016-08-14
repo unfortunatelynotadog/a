480d9c8509197e90c741e41b22917586728c83fb
coreyderosa <coreyderosa@gmail.com>
2016-08-13 18:41:37 -0600

    .attr method
    updated selector to #picture.  # becuase the HTML has an id=picture so
    #=id.  in .attr("src") states what about the picture element we want to
    change...then we add , "the new source of the picture")

--- JavaScript2.6.js
+++ JavaScript2.6.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 	$("h1").click(function() {
-		$("p").replaceWith("<h2>new text</h2>");
+		$("#picture").attr("src", "https://www.magicmurals.com/tools/phpThumb/phpThumb.php?src=/media/catalog/product/O/T/OTB-DNI0000021_3.jpg&fltr%5B%5D=wmi%7CwatermarkMM2.png%7CB");
 		
 	});
 
