8a46ed02a662c8abcfb08733eea4631d172314b6
coreyderosa <coreyderosa@gmail.com>
2016-08-13 20:05:34 -0600

    lineNumb++
    incrementing the background of red every time h1 is clicked

--- JavaScript2.8.js
+++ JavaScript2.8.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
 	var lineNum = 0;
 
+	
 	$("h1").click(function() {
-		$("p").eq(lineNum).css("background-color", "red");
-		
+
+		$("p").css("background-color", "yellow");	
+		$("p").eq(lineNum).css("background-color", "red");	
+		lineNum++;		
 		
 	});
 
