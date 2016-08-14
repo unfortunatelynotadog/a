9bc7a50000a4503f702a84081b821dd6b67d988a
coreyderosa <coreyderosa@gmail.com>
2016-08-13 19:55:43 -0600

    Combing 2 variables
    combined hText and text1 as one string of text to insert into .text
    method

--- JavaScript2.8.js
+++ JavaScript2.8.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
 	var hText = $("#head1").text();
-	
+	var text1 = "The heading text is ";
+	var text2 = text1 + hText;
 
 	$("h1").click(function() {
-		$("p").text(hText);
+		$("p").text(text2);
 		
 		
 	});
